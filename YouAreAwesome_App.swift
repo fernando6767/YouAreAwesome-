b7abb436ed4ad5e08e37f988a6c3379f32da97fa
fernando6767 <ferjorodriguez300@gmail.com>
2025-12-15 21:17:38 -0800

    YOU ARE AWESOME

---
+++ YouAreAwesome_App.swift
@@ -0,0 +1,18 @@
+//
+//  YouAreAwesome_App.swift
+//  YouAreAwesome!
+//
+//  Created by SILVA, NICOLE on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
